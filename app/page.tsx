94f6b190d79022cf6df9e2bd789c66afc307fbfb
Rizky Ramadhan <ramarizdev@gmail.com>
2025-08-13 11:25:47 +0700

    feat: add header

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import Header from '@/components/Header';
+
 export default function Home() {
-  return <div className=''></div>;
+  return (
+    <div className='min-h-screen bg-neutral-100'>
+      <Header />
+    </div>
+  );
 }
---
+++ components/Header.tsx
@@ -0,0 +1,33 @@
+import { Plus } from 'lucide-react';
+import { Button } from './ui/button';
+import { SignedIn, UserButton } from '@clerk/nextjs';
+import { Input } from '@/components/ui/input';
+
+const Header = () => {
+  return (
+    <header className='bg-white h-16 px-4'>
+      <div className='max-w-7xl flex justify-between items-center gap-2 md:gap-4 h-full mx-auto'>
+        <div className='flex items-center gap-2 md:gap-4'>
+          <div className='text-xl md:text-3xl font-black text-primary'>
+            Quotes
+          </div>
+          <Input
+            className='bg-neutral-100 border-none'
+            placeholder='Search quotes...'
+          />
+        </div>
+        <div className='flex items-center gap-2 md:gap-4'>
+          <Button size='sm'>
+            <Plus />
+            <span className='hidden md:inline'>Add Quote</span>
+          </Button>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
